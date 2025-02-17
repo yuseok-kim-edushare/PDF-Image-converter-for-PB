bebad0bb43577f720542eeb714460b7d0201a125
김유석(Yu Seok Kim) <yuseok.kim@vilac.co.kr>
2025-02-17 13:59:32 +0900

    🐛 Fix class structure to instantiation

--- PdfConverter.cs
+++ PdfConverter.cs
@@ -9,11 +9,20 @@ using PdfSharp.Pdf.IO;
 namespace PdfToImageConverter
 {
     [ComVisible(true)]
+    [Guid("B8B9E0C1-D513-4D8A-B11F-4A10E3D0C1A9")]
+    public interface IPdfConverter
+    {
+        [ComVisible(true)]
+        string ConvertPdfToImage(string pdfPath, string outputPath, int dpi);
+    }
+
+    [ComVisible(true)]
     [Guid("A8B9E0C1-D513-4D8A-B11F-4A10E3D0C1A8")]
     [ClassInterface(ClassInterfaceType.None)]
-    public class PdfConverter
+    [ProgId("PdfToImageConverter.PdfConverter")]
+    public class PdfConverter : IPdfConverter
     {
-        private static void EnsureDirectoryExists(string filePath)
+        private void EnsureDirectoryExists(string filePath)
         {
             string directory = Path.GetDirectoryName(filePath);
             if (!string.IsNullOrEmpty(directory) && !Directory.Exists(directory))
@@ -23,7 +32,7 @@ namespace PdfToImageConverter
         }
 
         [ComVisible(true)]
-        public static string ConvertPdfToImage(string pdfPath, string outputPath, int dpi = 300)
+        public string ConvertPdfToImage(string pdfPath, string outputPath, int dpi = 300)
         {
             try
             {
