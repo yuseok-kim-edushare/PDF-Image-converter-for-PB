1d20bce817fc20033e18f871f3d633fbf2e345c6
김유석(Yu Seok Kim) <yuseok.kim@vilac.co.kr>
2025-03-14 16:31:13 +0900

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PdfToImageConverter")]
 [assembly: AssemblyDescription("PDF to Image Converter COM Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("YuSeok Kim")]
 [assembly: AssemblyProduct("PdfToImageConverter")]
 [assembly: AssemblyCopyright("Copyright © 2025")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("YuSeok Kim")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(true)]
 [assembly: Guid("7402F27D-79BB-44EA-92B4-B6492F74C6EC")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")] 
+[assembly: AssemblyVersion("1.2.2.59")]
+[assembly: AssemblyFileVersion("1.2.2.59")] 
